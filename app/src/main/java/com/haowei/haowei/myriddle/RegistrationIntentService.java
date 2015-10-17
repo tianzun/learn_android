616f000707ea22e18c2796bd1643d1b9118a14ad
tianzun <tianzun@github.com>
2015-10-17 00:04:57 -0700

    rather than using parse, directly use google cloud messaging API to get push notification

---
+++ app/src/main/java/com/haowei/haowei/myriddle/RegistrationIntentService.java
@@ -0,0 +1,75 @@
+package com.haowei.haowei.myriddle;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.gcm.GcmPubSub;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import java.io.IOException;
+
+/**
+ * Created by haowei on 10/14/15.
+ */
+public class RegistrationIntentService extends IntentService {
+
+    private static final String TAG = "RegIntentService";
+
+    private static final String[] TOPICS = {"global"};
+
+    /* Creates an IntentService.  Invoked by your subclass's constructor.*/
+    public RegistrationIntentService() {
+        super(TAG);
+    }
+
+
+    private String getInstanceIDToken() {
+
+        String iid = InstanceID.getInstance(this).getId();
+        Log.i(TAG, "iid is:" + iid);
+
+        String authorizedEntity = ""; // Project id from Google Developers Console
+        String scope = "GCM"; // e.g. communicating using GCM, but you can use any
+        // URL-safe characters up to a maximum of 1000, or
+        // you can also leave it blank.
+        String token = null;
+        try {
+            token = InstanceID.getInstance(this).getToken(
+                    authorizedEntity, GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+            Log.i(TAG, "token is:" + token);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return token;
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String token = getInstanceIDToken();
+
+        if (token != null) {
+            try {
+                Log.i(TAG, "Now start register token");
+                subscribeTopics(token);
+                Log.i(TAG, "registration success");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void subscribeTopics(String token) throws IOException {
+        GcmPubSub pubSub = GcmPubSub.getInstance(this);
+        Log.i(TAG, "created a GcmPubSub, " + TOPICS.toString());
+        for (String topic : TOPICS) {
+            Log.i(TAG, "subscribing topic, topic is " + topic);
+            pubSub.subscribe(token, "/topics/" + topic, null);
+        }
+    }
+}
